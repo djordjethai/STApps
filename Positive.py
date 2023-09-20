5f3557fcf6e8cfa078c64776d46c6ae92911a4a1
Perun98 <nemanja.perun98@gmail.com>
2023-09-20 09:24:33 +0200

    txtth

--- Positive.py
+++ Positive.py
@@ -19,21 +19,17 @@ def main():
     st.info(
         f'Your username is {name} and email is {email} and access level is {access_level}.')
     st.caption(
-        "Sa leve strane mozete da birate aplikacije. Skrolujte po potrebi da vidite sve aplikacije.")
-    st.caption(
-        "Za vecinu aplikacija mozete da birate jezicki model i temperaturu.")
+        """**Sa leve strane možete da birate aplikacije. Skrolujte po potrebi da vidite sve aplikacije.
+        Za većinu aplikacija mozete da birate jezički model i temperaturu.**""")
     st.divider()
-    st.caption("Aplikacije se snabdevene razlicitim alatima.")
-    st.caption("Na primer, za MP3 to TXT koristimo OpenAI Audio model - Whisper,")
-    st.caption(
-        "za Koder koristimo Pinecone index naucen sa sajtova LangChain i Streamlit,")
-    st.caption(
-        "za Pisi u stilu koristimo Pinecone naucen stilovima razlicitih osoba,")
-    st.caption(
-        "Multi Tool Chatbot ima pristup internetu i koristi podatke iz Pinecone indeksa.")
-    st.caption(
-        "App Zapisnik kreira zapisnike ili sumarizacije na osnovu transkripta audio fajla.")
-
+    st.caption("""
+               Aplikacije su snabdevene razlicitim alatima. 
+               Na primer, za MP3 to TXT koristimo OpenAI Audio model - Whisper,
+               za Koder koristimo Pinecone index naucen sa sajtova LangChain i Streamlit,
+               za Pisi u stilu koristimo Pinecone naucen stilovima razlicitih osoba,
+               Multi Tool Chatbot ima pristup internetu i koristi podatke iz Pinecone indeksa.
+               App Zapisnik kreira zapisnike ili sumarizacije na osnovu transkripta audio fajla.
+               """)
     st.sidebar.success("Select an App above.")
 
 
