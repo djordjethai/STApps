27042afc5d66a6f15c4859bc77828cad25c6881a
Perun98 <nemanja.perun98@gmail.com>
2023-09-21 12:35:48 +0200

    wef

--- pages/Pisi_u_stilu_FT.py
+++ pages/Pisi_u_stilu_FT.py
@@ -122,7 +122,7 @@ def main():
         st.caption("Temperatura za stil treba de je što bliže 1.0")
         st.session_state.thold = st.slider(
             'Set relevance (0=any, 1=strict)', 0.0, 1.0, step=0.1, value=0.5)
-        st.caption("Relevance za temu određuje koji dokmenti će se korsititi iz indeksa. Ako je vrednost 0.0 onda se koriste svi dokumenti, a za 1.0 samo oni koji su najrelevantniji.")
+        st.caption("Relevantnost za temu određuje koji dokmenti će se korsititi iz indeksa. Ako je vrednost 0.0 onda se koriste svi dokumenti, a za 1.0 samo oni koji su najrelevantniji.")
 
     # define model, vestorstore and retriever
     llm = ChatOpenAI(model_name=st.session_state.model, temperature=st.session_state.temp,
