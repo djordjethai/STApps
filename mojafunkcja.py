dd8167c89088e43b773ca7e46d1ddeafd743c661
Perun98 <nemanja.perun98@gmail.com>
2023-10-24 11:00:14 +0200

    dd

--- mojafunkcja.py
+++ mojafunkcja.py
@@ -115,7 +115,7 @@ def st_style():
                 section > div {padding-top: 1rem;}
 
                 [data-testid="stSidebar"] { 
-                    background: linear-gradient(to right, #fbfdff, #f7fbff); 
+                    background: linear-gradient(to right, #f5f5f5, #e8e8e8); 
                     box-shadow: 15px 15px 10px rgba(0,0,0,0.1);
                     border-radius: 10px;
                 }   
@@ -125,7 +125,7 @@ def st_style():
                     margin-top: -6rem;
                     border-style: solid;
                     border-width: 20px;
-                    border-image: linear-gradient(to right, #fbfdff, #f7fbff) 1;
+                    border-image: linear-gradient(to right, #f5f5f5, #e8e8e8) 1;
                 }
 
                 .stSlider {
